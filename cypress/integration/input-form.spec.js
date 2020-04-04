19cdca043ee55428fdcfd1bbe8ac84abab6e2335
Hamza Takouit <hamzatakouit@gmail.com>
2020-04-04 22:30:24 +0100

    test: deletion tests

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -1,4 +1,9 @@
 describe("Input form", () => {
+  const todoText1 = "Finish tp";
+  const todoText2 = "Practice clean code";
+  const todoText3 = "Learn ES6";
+  const todoText4 = "Learn Cypress";
+
   beforeEach(() => {
     cy.visit("/");
   });
@@ -8,16 +13,10 @@ describe("Input form", () => {
   });
 
   it("accepts input", () => {
-    const typedText = "Practice clean code";
-    cy.get(".new-todo").type(typedText).should("have.value", typedText);
+    cy.get(".new-todo").type(todoText4).should("have.value", todoText4);
   });
 
   context("Form submission", () => {
-    const todoText1 = "Finish tp";
-    const todoText2 = "Practice clean code";
-    const todoText3 = "Learn ES6";
-    const todoText4 = "Learn Cypress";
-
     it("Adds 1 new todo on submit", () => {
       cy.get(".new-todo")
         .type(todoText1)
@@ -52,6 +51,73 @@ describe("Input form", () => {
         .and("contain", todoText2)
         .and("contain", todoText3)
         .and("contain", todoText4);
+    });
+  });
+
+  context("List items actions", () => {
+    it("check if remove button is not visible", () => {
+      cy.get(".new-todo")
+        .type(todoText4)
+        .type("{enter}")
+        .should("have.value", "");
+      cy.get(`.todo-list li`).eq(0).find(".destroy").should("not.be.visible");
+    });
+
+    it("checks if the remove button is hidden",()=>{
+      cy.get(".new-todo")
+        .type(todoText1)
+        .type("{enter}")
+        .should("have.value", "");
+      cy.get(".new-todo")
+        .type(todoText2)
+        .type("{enter}")
+        .should("have.value", "");
+      cy.get(".new-todo")
+        .type(todoText3)
+        .type("{enter}")
+        .should("have.value", "");
+        cy.get(`.todo-list li`).find('.destroy').should('not.visible')
+    })
+
+    it("shows the remove button on hover", () => {
+      cy.get(".new-todo")
+        .type(todoText4)
+        .type("{enter}")
+        .should("have.value", "");
+      cy.get(`.todo-list li`)
+        .eq(0)
+        .get(".destroy")
+        .invoke("show")
+        .should("be.visible");
+    });
+
+    it("Removes 1 element", () => {
+      cy.get(".new-todo")
+        .type(todoText4)
+        .type("{enter}")
+        .should("have.value", "");
+      cy.get(`.todo-list li`).eq(0).get(".destroy").invoke("show").click();
+    });
+
+    it("Removes multiple elements", () => {
+      cy.get(".new-todo")
+        .type(todoText1)
+        .type("{enter}")
+        .should("have.value", "");
+      cy.get(".new-todo")
+        .type(todoText2)
+        .type("{enter}")
+        .should("have.value", "");
+      cy.get(".new-todo")
+        .type(todoText3)
+        .type("{enter}")
+        .should("have.value", "");
+      cy.get(`.todo-list li`).eq(2).find('.destroy').invoke('show').click();
+      cy.get(`.todo-list li`).contains(todoText3).should('not.exist')
+
+      cy.get(`.todo-list li`).eq(1).find('.destroy').invoke('show').click();
+      cy.get(`.todo-list li`).contains(todoText2).should('not.exist')
+      cy.get(`.todo-list li`).should("have.length", 1)
     });
   });
 });
