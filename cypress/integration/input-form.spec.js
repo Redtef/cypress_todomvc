78fffa1797bc4e0c7246f59605c63fdfebcef434
Hamza Takouit <hamzatakouit@gmail.com>
2020-04-07 14:51:34 +0100

    chore:moved functions to cypress commands

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -6,7 +6,7 @@ describe("Input form", () => {
   context("Todo addition", () => {
     it("Should add new todo", () => {
       const todoText = "Finish tp";
-      insertTodoFromList([todoText]);
+      cy.insertTodos([todoText]);
       cy.get(".todo-list li").should("have.length", 1).and("contain", todoText);
     });
 
@@ -16,7 +16,7 @@ describe("Input form", () => {
       const todoText3 = "Learn ES6";
       const todoText4 = "Learn Cypress";
 
-      insertTodoFromList([todoText1, todoText2, todoText3, todoText4]);
+      cy.insertTodos([todoText1, todoText2, todoText3, todoText4]);
       cy.get(".todo-list li")
         .should("have.length", 4)
         .and("contain", todoText1)
@@ -29,8 +29,8 @@ describe("Input form", () => {
   context("Todo Deletion", () => {
     it("Removes 1 element", () => {
       const todoText1 = "Finish tp";
-      insertTodoFromList([todoText1]);
-      deleteElements([todoText1]);
+      cy.insertTodos([todoText1]);
+      cy.deleteTodos([todoText1]);
       cy.get(".todo-list").find("li").should("have.length", 0);
     });
 
@@ -40,10 +40,10 @@ describe("Input form", () => {
       const todoText3 = "Learn ES6";
       const todoText4 = "Learn Cypress";
 
-      insertTodoFromList([todoText1, todoText2, todoText3, todoText4]);
+      cy.insertTodos([todoText1, todoText2, todoText3, todoText4]);
 
-      deleteElements([todoText1, todoText2]);
-      shouldElementNotContains('.todo-list li',[todoText1, todoText2])
+      cy.deleteTodos([todoText1, todoText2]);
+      cy.shouldElementNotContains('.todo-list li',[todoText1, todoText2])
       cy.get(`.todo-list li`)
         .should("have.length", 2)
         .and("contain", todoText3)
@@ -52,24 +52,7 @@ describe("Input form", () => {
   });
 });
 
-function shouldElementNotContains(element,todoList){
-  for (const todo of todoList) {
-    cy.get(element).contains(todo).should("not.exist");
-  }
-}
 
-function deleteElements(todoList){
-  for (const todo of todoList) {
-    cy.get(`.todo-list li`)
-        .contains(todo)
-        .parent()
-        .find(".destroy")
-        .invoke("show")
-        .click();
-  }
-}
-function insertTodoFromList(todoList) {
-  for (const todo of todoList) {
-    cy.get(".new-todo").type(todo).type("{enter}");
-  }
-}
+
+
+
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,24 @@
+Cypress.Commands.add("insertTodos", (todoList) => {
+  for (const todo of todoList) {
+    cy.get(".new-todo").type(todo).type("{enter}");
+  }
+});
+
+Cypress.Commands.add("deleteTodos", (todoList) => {
+  for (const todo of todoList) {
+    cy.get(`.todo-list li`)
+      .contains(todo)
+      .parent()
+      .find(".destroy")
+      .invoke("show")
+      .click();
+  }
+});
+Cypress.Commands.add("shouldElementNotContains", (element,todoList) => {
+    for (const todo of todoList) {
+        cy.get(element).contains(todo).should("not.exist");
+      }
+  });
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
