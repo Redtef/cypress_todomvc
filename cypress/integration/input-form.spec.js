883f8b0a80e6f7d4e255359e47b43151ff64938c
Hamza Takouit <hamzatakouit@gmail.com>
2020-04-07 11:41:34 +0100

    chore:code refactor & remove code duplication

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -1,25 +1,22 @@
 describe("Input form", () => {
-  const todoText1 = "Finish tp";
-  const todoText2 = "Practice clean code";
-  const todoText3 = "Learn ES6";
-  const todoText4 = "Learn Cypress";
-
   beforeEach(() => {
     cy.visit("/");
   });
 
   context("Todo addition", () => {
     it("Should add new todo", () => {
-      const todoList = [todoText1];
-      insertTodoFromList(todoList);
-      cy.get(".todo-list li")
-        .should("have.length", 1)
-        .and("contain", todoText1);
+      const todoText = "Finish tp";
+      insertTodoFromList([todoText]);
+      cy.get(".todo-list li").should("have.length", 1).and("contain", todoText);
     });
 
     it("Should add multiple new todos", () => {
-      const todoList = [todoText1, todoText2, todoText3, todoText4];
-      insertTodoFromList(todoList);
+      const todoText1 = "Finish tp";
+      const todoText2 = "Practice clean code";
+      const todoText3 = "Learn ES6";
+      const todoText4 = "Learn Cypress";
+
+      insertTodoFromList([todoText1, todoText2, todoText3, todoText4]);
       cy.get(".todo-list li")
         .should("have.length", 4)
         .and("contain", todoText1)
@@ -31,40 +28,46 @@ describe("Input form", () => {
 
   context("Todo Deletion", () => {
     it("Removes 1 element", () => {
-      const todoList = [todoText1];
-      insertTodoFromList(todoList);
-      cy.get(`.todo-list li`)
-        .contains(todoText1)
-        .get(".destroy")
-        .invoke("show")
-        .click();
+      const todoText1 = "Finish tp";
+      insertTodoFromList([todoText1]);
+      deleteElements([todoText1]);
       cy.get(".todo-list").find("li").should("have.length", 0);
     });
 
     it("Removes multiple elements", () => {
-      const todoList = [todoText1, todoText2, todoText3, todoText4];
-      insertTodoFromList(todoList);
+      const todoText1 = "Finish tp";
+      const todoText2 = "Practice clean code";
+      const todoText3 = "Learn ES6";
+      const todoText4 = "Learn Cypress";
 
-      cy.get(`.todo-list li`)
-        .contains(todoText1)
-        .parent()
-        .find(".destroy")
-        .invoke("show")
-        .click();
-      cy.get(`.todo-list li`)
-        .contains(todoText2)
-        .parent()
-        .find(".destroy")
-        .invoke("show")
-        .click();
+      insertTodoFromList([todoText1, todoText2, todoText3, todoText4]);
 
-      cy.get(`.todo-list li`).contains(todoText1).should("not.exist");
-      cy.get(`.todo-list li`).contains(todoText2).should("not.exist");
-      cy.get(`.todo-list li`).should("have.length", 2).and("contain",todoText3).and("contain",todoText4);
+      deleteElements([todoText1, todoText2]);
+      shouldElementNotContains('.todo-list li',[todoText1, todoText2])
+      cy.get(`.todo-list li`)
+        .should("have.length", 2)
+        .and("contain", todoText3)
+        .and("contain", todoText4);
     });
   });
 });
 
+function shouldElementNotContains(element,todoList){
+  for (const todo of todoList) {
+    cy.get(element).contains(todo).should("not.exist");
+  }
+}
+
+function deleteElements(todoList){
+  for (const todo of todoList) {
+    cy.get(`.todo-list li`)
+        .contains(todo)
+        .parent()
+        .find(".destroy")
+        .invoke("show")
+        .click();
+  }
+}
 function insertTodoFromList(todoList) {
   for (const todo of todoList) {
     cy.get(".new-todo").type(todo).type("{enter}");
